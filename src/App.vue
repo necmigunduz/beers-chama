b3cfe52730550a088be4ce18b430970ecab27390
necmigunduz <necmigunduz@gmail.com>
2022-04-11 10:45:58 +0300

    Add functions and rendering to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,68 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+    <v-card>
+      <v-card-title>
+        <h1 outlined color="black">Search Beer</h1>
+        <v-spacer></v-spacer>
+        <v-text-field
+          type="text"
+          v-model.trim="searchInqury"
+          placeholder="Enter beer name here"
+          @keyup="getAllBeers"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+      </v-card-title>
+      <v-card-text v-if="searchInqury === ''">
+        Enter a word into the search bar!
+      </v-card-text>
+      <v-list width="40%" class="d-flex flex-row m-9" v-else-if="beers.length">
+        <v-list-item class="m-9" v-for="b in beers" :key="b.id">
+          <v-list class="w-2">
+            <v-img
+              v-bind:src="b.image_url"
+              alt="beer-img"
+              max-width="50"
+              height="auto"
+            >
+            </v-img>
+            <h3>{{ b.name }}</h3>
+            <v-list-item-text>{{ b.description }}</v-list-item-text>
+          </v-list>
+        </v-list-item>
+      </v-list>
+      <v-card-text v-else>
+        No beer found! Try another word in search!
+      </v-card-text>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  name: "Beer-Search",
+  data() {
+    return {
+      beers: [],
+      searchInqury: "",
+    };
   },
-
-  data: () => ({
-    //
-  }),
+  methods: {
+    getAllBeers() {
+      const url = "https://api.punkapi.com/v2/beers";
+      fetch(url)
+        .then((response) => response.json())
+        .then((res) => {
+          if (this.searchInqury) {
+            this.beers = res.filter((beer) =>
+              beer.name.toLowerCase().includes(this.searchInqury.toLowerCase())
+            );
+          } else {
+            this.beers = res;
+          }
+        });
+    },
+    created() {
+      this.getAllBeers();
+    },
+  },
 };
 </script>
